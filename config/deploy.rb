e068b343691cd5e87c7c877d66fbf5b39dfec521
Lucas <femani.lucas@hotmail.com>
2023-08-08 17:32:13 -0300

    rbenv ruby version update

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ lock "~> 3.17.3"
 # set :ssh_options, verify_host_key: :secure
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '3.1.2'
+set :rbenv_ruby, '3.2.2'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
